3a2c6d0a577845fbe78c830af42ea5950b14eceb
vgonisanz <vgonisanz@gmail.com>
2017-12-31 14:01:26 +0100

    steamlike: Changed rotation axi

--- examples/steamlike/steamlike.cpp
+++ examples/steamlike/steamlike.cpp
@@ -85,7 +85,7 @@ void drawTriangle()
     glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    glRotatef(-(float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
     glBegin(GL_TRIANGLES);
     glColor3f(1.f, 0.f, 0.f);
     glVertex3f(-0.6f, -0.4f, 0.f);
